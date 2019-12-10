e5256c75deed1bc007afeb89668666fe40492bd7
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2019-12-10 19:28:36 +0100

    Add saveDocument feature to document service

--- textedit-core/source/textedit/services/documentservice.d
+++ textedit-core/source/textedit/services/documentservice.d
@@ -12,10 +12,17 @@ interface IDocumentService
 	/** 
 	 * Opens a text document.
 	 * Params:
-	 *    filename = The path to the file to open.
+	 *   filename = The path to the file to open.
 	 * Returns: The text document.
 	 */
 	TextDocument openDocument(string filename);
+
+	/** 
+	 * Saves a document.
+	 * Params:
+	 *    document = The document to save.
+	 */
+	void saveDocument(const TextDocument document);
 }
 
 class DocumentService : IDocumentService
@@ -32,10 +39,25 @@ class DocumentService : IDocumentService
 	unittest
 	{
 		auto service = new DocumentService;
-		auto path = tempDir ~ "/file.txt";
+		auto path = tempDir ~ "/textedit-unittest-file.txt";
 		write(path, "foobar");
 		const document = service.openDocument(path);
 		assert(document.path == path);
 		assert(document.content == "foobar");
+	}
+
+	override void saveDocument(const TextDocument document) const
+	{
+		write(document.path, document.content);
+	}
+
+	@("saveDocument saves a document")
+	unittest
+	{
+		auto service = new DocumentService;
+		auto path = tempDir ~ "/textedit-unittest-file.txt";
+		const document = new TextDocument(path, "foobar");
+		service.saveDocument(document);
+		assert(path.readText == "foobar");
 	}
 }
